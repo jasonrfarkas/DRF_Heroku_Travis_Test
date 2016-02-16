7979fde3649d3879183717315c6117c5e81c1b4b
Jason <jasonrfarkas@gmail.com>
2016-02-16 16:20:53 -0500

    initial

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/SruliF/Documents/coding/Fueled/Learning/DRF_scratch_2/tutorial/env/bin/python3.4
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
